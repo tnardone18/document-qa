608edd7c35def80b87a060b9d3c04a95437b7ed2
tnardone18 <tcnardon@syr.edu>
2026-02-16 18:53:45 +0000

    adding lab5

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ st.title('IST 488 Labs')
 lab1 = st.Page('Labs/Lab1.py', title = 'Lab1', icon = '💻')
 lab2 = st.Page('Labs/Lab2.py', title = 'Lab2', icon = '💻')
 lab3 = st.Page('Labs/Lab3.py', title = 'Lab3', icon = '💻')
-lab4 = st.Page('Labs/Lab4.py', title = 'Lab4', icon = '💻', default = True)
+lab4 = st.Page('Labs/Lab4.py', title = 'Lab4', icon = '💻')
+lab5 = st.Page('Labs/Lab5.py', title = 'Lab5', icon = '💻', default = True)
 
-pg = st.navigation([lab1, lab2, lab3, lab4])
+
+pg = st.navigation([lab1, lab2, lab3, lab4, lab5])
 st.set_page_config(page_title = 'IST 488 Labs',
                    initial_sidebar_state='expanded')
 pg.run()
